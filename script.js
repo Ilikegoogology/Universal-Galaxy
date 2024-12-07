3ba4e4ef9a46fca194a0722cc342ab73a4b55921
Ilikegoogology <142138596+Ilikegoogology@users.noreply.github.com>
2024-12-07 21:45:32 +0200

    Initial release

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Everything at the initial input 
 let galaxies = 0;
 let galaxyGain = 0.01;
 let upgrade1Level = 0;
@@ -5,11 +6,12 @@ let upgrade2Level = 0;
 const upgrade1Cost = 10;
 const upgrade2Cost = 25;
 
+// - How much galaxy increments 
 function incrementGalaxies() {
     galaxies += galaxyGain * (2 ** upgrade1Level) * (4 ** upgrade2Level);
     document.getElementById("galaxyCount").innerText = galaxies.toFixed(2);
 }
-
+// - Upgrades
 function buyUpgrade1() {
     if (galaxies >= upgrade1Cost) {
         galaxies -= upgrade1Cost;
@@ -25,5 +27,5 @@ function buyUpgrade2() {
         document.getElementById("upgrade2Level").innerText = upgrade2Level;
     }
 }
-
+// How much time it takes to increment. 
 setInterval(incrementGalaxies, 150);
