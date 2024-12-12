32bf2b6f13aee6bc1a01caf9a004712e6412258b
Ilikegoogology <142138596+Ilikegoogology@users.noreply.github.com>
2024-12-12 22:54:42 +0200

    0.0α-5 | Added a soft cap to the U2+A Randomization between 1/64 and 1.4
    U2 softcap at Lv.11
    U2 softcap formula: U2 Multiplier/2147483647+Galaxies

--- script.js
+++ script.js
@@ -6,9 +6,22 @@ let upgrade2Level = 0;
 const upgrade1Cost = 10;
 const upgrade2Cost = 25;
 
+// Function to generate a random number between 1/64 and 1.4
+function getRandomMultiplier() {
+    return Math.random() * (1.4 - (1/64)) + (1/64);
+}
+
 // How much galaxy increments 
 function incrementGalaxies() {
-    galaxies += galaxyGain * (2 ** upgrade1Level) * (4 ** upgrade2Level);
+    let multiplier = 1;
+    if (upgrade1Level <= 10) {
+        multiplier *= getRandomMultiplier() ** upgrade1Level;
+    } else {
+        multiplier *= (getRandomMultiplier() ** 10) / (2_147_483_648 + galaxies);
+    }
+    multiplier *= 1.1 ** upgrade2Level;
+
+    galaxies += galaxyGain * multiplier;
     document.getElementById("galaxyCount").innerText = galaxies.toFixed(2);
 }
 
@@ -17,7 +30,7 @@ function buyUpgrade1() {
     if (galaxies >= upgrade1Cost) {
         galaxies -= upgrade1Cost;
         upgrade1Level += 1;
-        document.getElementById("upgrade1Level").innerText = `Level: ${upgrade1Level} (Currently ${2 * upgrade1Level})`;
+        document.getElementById("upgrade1Level").innerText = `Level: ${upgrade1Level} (Currently ${Math.round(getRandomMultiplier() ** Math.min(upgrade1Level, 10) * 100) / 100})`;
     }
 }
 
@@ -25,7 +38,7 @@ function buyUpgrade2() {
     if (galaxies >= upgrade2Cost) {
         galaxies -= upgrade2Cost;
         upgrade2Level += 1;
-        document.getElementById("upgrade2Level").innerText = `Level: ${upgrade2Level} (Currently x${4 ** upgrade2Level})`;
+        document.getElementById("upgrade2Level").innerText = `Level: ${upgrade2Level} (Currently x${Math.round(1.1 ** upgrade2Level * 100) / 100})`;
     }
 }
 
